ba85b6f79c31f8501aed7151ec2b795ba8abe721
Achbar Wahyudhi <achbarwahyudhi2001@gmail.com>
2023-11-10 13:59:01 +0700

    Tambah profile user dan admin

--- webLogistik/resources/views/admin/template/header.blade.php
+++ webLogistik/resources/views/admin/template/header.blade.php
@@ -123,7 +123,7 @@
                             <span class="d-none d-lg-inline-flex">Eka Putri</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
-                            <a href="#" class="dropdown-item">My Profile</a>
+                            <a href="{{route('profile.index')}}" class="dropdown-item">My Profile</a>
                             <a href="#" class="dropdown-item">Settings</a>
                             <a href="logout.php" class="dropdown-item">Log Out</a>
                         </div>
