ce4e551af965b0c7b81e8a98fa669c13bcc60ef7
michailtjhang <michailaja@gmail.com>
2023-11-13 14:58:24 +0700

    Tidying up coding on the homepage

--- webLogistik/resources/views/homepage/dashboardhome.blade.php
+++ webLogistik/resources/views/homepage/dashboardhome.blade.php
@@ -252,7 +252,7 @@
             <div class="row ">
                 <div class="col-lg-3 col-md-6 mx-auto " data-aos="zoom-in-up" data-aos-delay="100">
                     <div class="member">
-                        <div class="pic"><img src="{{asset('homepage/img/team-1.jpg')}}" alt=""></div>
+                        <div class="pic"><img src="{{asset('homepage/img/Author/team-1.jpg')}}" alt=""></div>
                         <div class="details">
                             <h4>Khoirul Huda</h4>
                             <span>Chief Executive Officer</span>
@@ -268,9 +268,9 @@
 
                 <div class="col-lg-3 col-md-6 mx-auto" data-aos="zoom-in-up" data-aos-delay="200">
                     <div class="member">
-                        <div class="pic"><img src="{{asset('homepage/img/team-2.jpg')}}" alt=""></div>
+                        <div class="pic"><img src="{{asset('homepage/img/Author/team-2.jpg')}}" alt=""></div>
                         <div class="details">
-                            <h4>Devia</h4>
+                            <h4>Devia Fitri N</h4>
                             <span>Frond End Developer</span>
                             <div class="social">
                                 <a href=""><i class="bi bi-twitter"></i></a>
@@ -284,15 +284,15 @@
 
                 <div class="col-lg-3 col-md-6 mx-auto" data-aos="zoom-in-up" data-aos-delay="300">
                     <div class="member">
-                        <div class="pic"><img src="{{asset('homepage/img/team-3.jpg')}}" alt=""></div>
+                        <div class="pic"><img src="{{asset('homepage/img/Author/michail.png')}}" alt=""></div>
                         <div class="details">
                             <h4>Michail</h4>
-                            <span>Leader Back End Developer</span>
+                            <span>Back End Developer</span>
                             <div class="social">
-                                <a href=""><i class="bi bi-twitter"></i></a>
-                                <a href=""><i class="bi bi-facebook"></i></a>
-                                <a href=""><i class="bi bi-instagram"></i></a>
-                                <a href=""><i class="bi bi-linkedin"></i></a>
+                                <a href="https://twitter.com/michailtj39"><i class="bi bi-twitter"></i></a>
+                                <a href="https://www.facebook.com/michail.tjhang"><i class="bi bi-facebook"></i></a>
+                                <a href="https://www.instagram.com/michailtj39/"><i class="bi bi-instagram"></i></a>
+                                <a href="https://www.linkedin.com/in/michailtj39/"><i class="bi bi-linkedin"></i></a>
                             </div>
                         </div>
                     </div>
@@ -302,9 +302,9 @@
             <div class="row">
                 <div class="col-lg-3 col-md-6 mx-auto" data-aos="zoom-in-up" data-aos-delay="400">
                     <div class="member">
-                        <div class="pic"><img src="{{asset('homepage/img/team-4.jpg')}}" alt=""></div>
+                        <div class="pic"><img src="{{asset('homepage/img/Author/team-4.jpg')}}" alt=""></div>
                         <div class="details">
-                            <h4>Angelina</h4>
+                            <h4>Angelina Yulfaningtyas</h4>
                             <span>Front End Developer</span>
                             <div class="social">
                                 <a href=""><i class="bi bi-twitter"></i></a>
@@ -318,9 +318,9 @@
 
                 <div class="col-lg-3 col-md-6 mx-auto" data-aos="zoom-in-up" data-aos-delay="500">
                     <div class="member">
-                        <div class="pic"><img src="{{asset('homepage/img/team-4.jpg')}}" alt=""></div>
+                        <div class="pic"><img src="{{asset('homepage/img/Author/team-3.jpg')}}" alt=""></div>
                         <div class="details">
-                            <h4>Achbar</h4>
+                            <h4>Achbar Wahyudhi</h4>
                             <span> Front End Developer</span>
                             <div class="social">
                                 <a href=""><i class="bi bi-twitter"></i></a>
