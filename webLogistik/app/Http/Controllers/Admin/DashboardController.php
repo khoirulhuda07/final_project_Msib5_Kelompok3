25e40777b467b87e37bf0c35c471748f922a1d46
michailtjhang <michailaja@gmail.com>
2023-11-14 17:03:00 +0700

    tidy up routes and dashboard views

--- webLogistik/app/Http/Controllers/Admin/DashboardController.php
+++ webLogistik/app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Pengiriman;
+use App\Models\Layanan;
+use App\Models\Kurir;
 
 class DashboardController extends Controller
 {
     // fungsi index
     public function index() {
-        return view("admin.dashboard");
+        $pengiriman = Pengiriman::join("penerima", "penerima_id", "=", "penerima.id")
+        ->join("layanan","layanan_id", "=", "layanan.id")
+        ->select('pengiriman.*', 'penerima.nama AS username', 'layanan.nama_layanan AS namaLayanan')
+        ->limit('5')
+        ->get();
+        $layanan = Layanan::all();
+        $kurir = Kurir::all();
+
+        return view("admin.dashboard", 
+        compact('pengiriman', 'layanan', 'kurir'));
     }
 }
--- webLogistik/resources/views/admin/template/header.blade.php
+++ webLogistik/resources/views/admin/template/header.blade.php
@@ -100,30 +100,11 @@
                 <div class="navbar-nav align-items-center ms-auto">
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                            <i class="fa fa-envelope me-lg-2"></i>
-                            <span class="d-none d-lg-inline-flex">Message</span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
-                            <a href="#" class="dropdown-item text-center">See all message</a>
-                        </div>
-                    </div>
-                    <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                            <i class="fa fa-bell me-lg-2"></i>
-                            <span class="d-none d-lg-inline-flex">Notificatin</span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
-                            <a href="#" class="dropdown-item text-center">See all notifications</a>
-                        </div>
-                    </div>
-                    <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                             <img class="rounded-circle me-lg-2" src="{{asset('admin/img/user-1.jpg')}}" alt="" style="width: 40px; height: 40px;">
                             <span class="d-none d-lg-inline-flex">Eka Putri</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                             <a href="{{route('profile.index')}}" class="dropdown-item">My Profile</a>
-                            <a href="#" class="dropdown-item">Settings</a>
                             <a href="logout.php" class="dropdown-item">Log Out</a>
                         </div>
                     </div>
