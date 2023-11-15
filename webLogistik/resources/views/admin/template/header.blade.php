9c23debef03cc52c1bb5d374946e4be179a96d7b
khoirulhuda07 <muhammadsirinmus123@gmail.com>
2023-11-15 13:50:36 +0700

    update sweetealert

--- webLogistik/resources/views/admin/template/header.blade.php
+++ webLogistik/resources/views/admin/template/header.blade.php
@@ -37,6 +37,8 @@
 </head>
 
 <body>
+    @include('sweetalert::alert')
+
     <div class="container-fluid position-relative bg-white d-flex p-0">
         <!-- Spinner Start -->
         <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
