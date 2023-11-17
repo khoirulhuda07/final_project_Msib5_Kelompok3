40a31acd0a65e428a47b3d78955a55791b865460
deviafnopiani <dviafnopiani@gmail.com>
2023-11-17 18:57:53 +0700

    menambahkan chart di dashboard admin

--- webLogistik/resources/views/admin/template/footer.blade.php
+++ webLogistik/resources/views/admin/template/footer.blade.php
@@ -2,11 +2,11 @@
     <div class="bg-light rounded-top p-4">
         <div class="row">
             <div class="col-12 col-sm-6 text-center text-sm-start">
-                &copy; <a href="{{url('admin/dashboard')}}">Logistik</a>, All Right Reserved. 
+                &copy; <a href="{{url('admin/dashboard')}}">Logistik</a>, Kelompok 3 Grup 2 Batch 5
             </div>
             <div class="col-12 col-sm-6 text-center text-sm-end">
                 <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                Designed By <a href="https://htmlcodex.com">HTML Codex</a>
+                Designed By <a href="">Kelompok 3 Grup 2 Batch 5</a>
             </div>
         </div>
     </div>
@@ -39,6 +39,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+
 
 <!-- Template Javascript -->
 <script src="{{asset('admin/js/main.js')}}"></script>
