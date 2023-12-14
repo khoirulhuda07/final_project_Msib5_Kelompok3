ea27eb005dc6eacf1e1404827893cdb8777cab8c
khoirulhuda07 <muhammadsirinmus123@gmail.com>
2023-12-14 09:00:22 +0700

    solved

---
+++ webLogistik/app/Http/Controllers/api/LacakController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\LacakResource;
+use Illuminate\Http\Request;
+use App\Models\kurir;
+use App\Models\Layanan;
+use App\Models\Paket;
+use App\Models\Penerima;
+use App\Models\Pengiriman;
+use App\Models\Pembayaran;
+use App\Models\Dompet;
+use App\Models\Users;
+
+class LacakController extends Controller
+{
+    //
+    public function index()
+    {
+        //
+        $pengiriman = pengiriman::all();
+
+        $kurir = kurir::all();
+        $penerima = penerima::all();
+        $layanan = layanan::all();
+        $pembayaran = pembayaran::all();
+        $dompet = dompet::all();
+        $paket = paket::all();
+        return LacakResource::collection($pengiriman, $penerima, $kurir, $layanan, $pembayaran, $dompet, $paket);
+    }
+}
